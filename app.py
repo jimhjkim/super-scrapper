b391b6eea76c02ebff6b76edf1530f2f413bc7d8
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-20 02:46:31 -0500

    Download csv file

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scraper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScraper")
 
 db = {}
-# app.run(debug=True)
-# app.env("development")
 
 
 @app.route("/")
@@ -27,6 +26,22 @@ def report():
     else:
         return redirect("/")
     return render_template("report.html", word=word, resultsCount=len(jobs), jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 
 if __name__ == "__main__":
