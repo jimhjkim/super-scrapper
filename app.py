cec6f23096e85919ed7d45b8117e12023b3cdfaf
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-20 02:12:21 -0500

    Incorporate jobs scraping logic

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
+from scraper import get_jobs
 
 app = Flask("SuperScraper")
 
+db = {}
 # app.run(debug=True)
 # app.env("development")
 
@@ -16,9 +18,15 @@ def report():
     word = request.args.get("word")
     if word:
         word = word.lower()
+        from_db = db.get(word)
+        if from_db:
+            jobs = from_db
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect("/")
-    return render_template("report.html", word=word)
+    return render_template("report.html", word=word, resultsCount=len(jobs))
 
 
 if __name__ == "__main__":
